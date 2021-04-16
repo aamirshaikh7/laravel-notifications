0bf934296705aaa5ea24652a8fa3c713acec5690
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-04-16 15:21:15 +0530

    creating subsription route and view

---
+++ resources/views/subscribe.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                Subscribe
+            </button>
+        </h2>
+    </x-slot>
+</x-app-layout>
